169fa23d110e91185bb3199d4348cd3ed93808ad
ValentinGlairot <valentin.glairot@viacesi.fr>
2025-02-20 12:25:11 +0100

    Delete includes directory

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-  <p>
-    © 2025 - CESI Ta Chance | 
-    <a href="mentions-legales.php">Mentions légales</a> | 
-    <a href="conditions-generales.php">Conditions générales</a> | 
-    <a href="politique-confidentialite.php">Politique de confidentialité</a> | 
-    Contact : contact@cesitachance.com
-  </p>
-</footer>
